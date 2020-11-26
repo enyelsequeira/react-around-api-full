feee1ace32ef0a56becc0a36b92b61f9dac85b16
enyelsequeira <enyelsequeira@hotmail.com>
2020-11-26 18:51:11 +0000

    adding celebrate to all project

--- back-end/routes/cards.js
+++ back-end/routes/cards.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   createCard,
@@ -11,9 +12,81 @@ const {
 const router = express.Router();
 // router === /cards, (logic)
 router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/likes/:id', likeCard);
-router.delete('/cards/likes/:id', deleteCardLike);
+router.post(
+  '/cards',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string()
+        .required()
+        .uri({ scheme: ['http', 'https'] }),
+      likes: Joi.array().items(Joi.string()),
+    }),
+  }),
+  createCard
+);
+router.delete(
+  '/cards/:cardId',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    params: Joi.object().keys({
+      cardId: Joi.string().required().alphanum(),
+    }),
+  }),
+  deleteCard
+);
+router.put(
+  '/cards/likes/:id',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    params: Joi.object().keys({
+      id: Joi.string().required().alphanum(),
+    }),
+  }),
+  likeCard
+);
+router.delete(
+  '/cards/likes/:id',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    params: Joi.object().keys({
+      id: Joi.string().required().alphanum(),
+    }),
+  }),
+  deleteCardLike
+);
 
 module.exports = router;
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const router = express.Router();
 const {
@@ -10,13 +11,95 @@ const {
 } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
-router.get('/users/me', auth, getUserInfo);
+router.get(
+  '/users/me',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+  }),
+  getUserInfo
+);
 
 // general user routes
-router.get('/users', auth, getUsers);
-router.get('/users/:id', auth, getOneUser);
-router.patch('/users/me', auth, updateProfile);
-router.patch('/users/me/avatar', auth, updateAvatar);
+router.get(
+  '/users',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+  }),
+  getUsers
+);
+router.get(
+  '/users/:id',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    params: Joi.object().keys({
+      id: Joi.string().required().alphanum(),
+    }),
+  }),
+  getOneUser
+);
+router.patch(
+  '/users/me',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+    }),
+  }),
+  updateProfile
+);
+router.patch(
+  '/users/me/avatar',
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        authorization: Joi.string()
+          .regex(
+            /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/
+          )
+          .required(),
+      })
+      .options({ allowUnknown: true }),
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .uri({ scheme: ['http', 'https'] }),
+    }),
+  }),
+  updateAvatar
+);
 
 module.exports = router;
 // http://localhost:3000/users/5f845d006c7b26589c33b82
